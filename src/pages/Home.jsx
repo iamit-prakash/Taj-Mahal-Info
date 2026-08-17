add4ce00db4b5628bed8c7b3f5f85fbfd6027585
AMIT PRAKASH <amitprakash0412@gmail.com>
2026-08-18 00:38:25 +0530

    Revert "added monument"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,8 +21,6 @@ export default function Home(){
           considered one of the most beautiful architectural masterpieces in
           history.
         </p>
-        <h3>Monument</h3>
-        <p>Thos is monuments</p>
         </div>
     )
 }
